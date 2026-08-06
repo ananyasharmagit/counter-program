27154a61476158a698ef07f0d4b309d1fa854f72
Ananya Sharma <ananyasharma.shss@gmail.com>
2026-08-06 17:42:49 +0530

    Delete 1.basics of js directory

--- 1.basics of js/script.js
+++
@@ -1,9 +0,0 @@
-console.log('hello world');
-console.log("i like momos");
-
-//window.alert('this is an alert');
-//window.alert("momos are the best invention");
-// this is a comment - the above are tools to print in console and show alert 
-/* multi line  comment */
-document.getElementById("my H1").textContent = "HELLO" ; // by this we can type in js which will display on the webpage using html ids
-document.getElementById("my P").textContent = "Yo how are you"; 
